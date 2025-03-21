519531be750cc00ab80a68cbc9c914e97818cbb0
ashutosh414 <ashutoshmittal414@gmail.com>
2025-03-21 19:05:08 +0530

    Day001+ minor fix

--- Day001_Basic.sql
+++ Day001_Basic.sql
@@ -2,18 +2,18 @@ set serveroutput on;
 -- select * from EMPLOYEES;
 
 set serveroutput on;
-declare 
+declare
     -- creating and declareing a variable name 'msg'
     msg varchar(30) := 'hello world ashutosh';
 BEGIN
-
+    -- Print the line
     dbms_output.PUT_LINE(msg);
 END;
 
-declare 
+declare
     -- creating and declareing a variable name 'msg'
     msg varchar(30) := 'hello world ashutosh';
 BEGIN
     -- concat variable with sring
-    dbms_output.PUT_LINE(msg || ' ' || 'friend of Ram');
+    DBMS_OUTPUT.PUT_LINE(msg || ' ' || 'friend of Ram');
 END;
